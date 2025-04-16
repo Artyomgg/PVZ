5adc5362d0ed0f114db78c37a73d1c1928efd33e
babyshark3343 <kahovskag@Gmail.com>
2025-04-16 18:56:06 +0300

    levels
    Сделал пока что начальное оформление уровней

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,9 @@
-function redirect(path){
-	if(path == 'levels'){
-
-	} else if(path == 'book'){
-		window.location.href = `pages/${path}.html`
-	} else if(path == 'dragon-grow'){
-		window.location.href = `pages/${path}.html`
-	}
+function redirect(path) {
+  if (path == "levels") {
+    window.location.href = `pages/${path}.html`;
+  } else if (path == "book") {
+    window.location.href = `pages/${path}.html`;
+  } else if (path == "dragon-grow") {
+    window.location.href = `pages/${path}.html`;
+  }
 }
