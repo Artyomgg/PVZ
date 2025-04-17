0f7bce7cb03fa010ce356116ad79cf4168b3790e
babyshark3343 <kahovskag@Gmail.com>
2025-04-17 19:38:54 +0300

    fix
    Пофиксил выбор уровней

--- assets/scripts/level-selection.js
+++ assets/scripts/level-selection.js
@@ -1,51 +1,39 @@
-const arrowright = document.querySelector('.level-arrows:last-child')
-const arrowleft = document.querySelector('.level-arrows:first-child')
-const levels = document.querySelector('.levels-list')
-const levelsprite = document.querySelector('.level-sprite')
-let clicks = 0
+const arrows = document.querySelectorAll(".level-arrows");
+const arrowLeft = arrows[0];
+const arrowRight = arrows[1];
+const levelText = document.querySelector(".level-label");
+const levelSprite = document.querySelector(".level-sprite");
+let level = 1;
 
 const islands = {
-    one: '../assets/img/Islands/island1.png',
-    two: '../assets/img/Islands/island2.png',
-    three: '../assets/img/Islands/island1.png', //потом вставить спрайты след. уровней
-    four: '../assets/img/Islands/island1.png',
-    five: '../assets/img/Islands/island1.png',
-}
+  1: "../assets/img/Islands/island1.png",
+  2: "../assets/img/Islands/island2.png",
+  3: "../assets/img/Islands/island1.png",
+  4: "../assets/img/Islands/island1.png",
+  5: "../assets/img/Islands/island1.png",
+};
 
-function updateLevelText() {
-    let levelnumber = clicks + 1
-    if (clicks >= 5) levelnumber = 5
-    levels.innerText = `Уровень ${levelnumber}` 
-}
-
-function updateIsland(clicks) {
-    alert('+') // проверка
-	if (clicks === 5) {
-		levelsprite.src = islands.five
-	} else if (clicks === 4) {
-		levelsprite.src = islands.four
-	} else if (clicks === 3) {
-		levelsprite.src = islands.three
-	} else if (clicks === 2) {
-		levelsprite.src = islands.two
-	} else if (clicks === 1) {
-		levelsprite.src = islands.one
-	} else {
-		levelsprite.src = islands.one
-	}
+function updateLevel() {
+  levelText.innerText = `Уровень ${level}`;
+  levelSprite.src = islands[level];
 }
 
 function setupIsland() {
-    updateLevelText()
-    updateIsland(clicks)
+  updateLevel();
 
-    function clickHandler() {
-        clicks++
-        updateLevelText() 
-        updateIsland(clicks) 
+  arrowRight.addEventListener("click", () => {
+    if (level < 5) {
+      level++;
+      updateLevel();
     }
+  });
 
-    arrowright.addEventListener('click', clickHandler)
+  arrowLeft.addEventListener("click", () => {
+    if (level > 1) {
+      level--;
+      updateLevel();
+    }
+  });
 }
 
-setupIsland()
+setupIsland();
