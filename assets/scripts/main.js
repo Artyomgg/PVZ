c1c15112b649c0b7ca71aff4f34ffe16fbfe3827
nomoroc <yuriydonskoi@gmail.com>
2025-04-24 22:02:16 +0300

    дракон в меню

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,23 @@ function redirect(path) {
     window.location.href = `pages/${path}.html`;
   }
 }
+
+const liItems = document.querySelectorAll('.container ul li')
+const gif = document.getElementById('gif')
+
+for (let i = 0; i < liItems.length; i++) {
+  liItems[i].addEventListener('mouseenter', function(e) {
+    gif.style.opacity = '1'
+    gif.style.transform = 'translate(-50%, -50%)'
+    gif.style.top = `${e.clientY - 170}px`
+    gif.style.left = `${gif.offsetLeft}px`
+  })
+
+  liItems[i].addEventListener('mousemove', function(e) {
+    gif.style.top = `${e.clientY - 170}px`
+  })
+
+  liItems[i].addEventListener('mouseleave', function() {
+    gif.style.opacity = '0'
+  })
+}
