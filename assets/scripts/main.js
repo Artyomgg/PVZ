6688df3d61ab96fce9b1adf0d1321006191d7c34
nomoroc <yuriydonskoi@gmail.com>
2025-04-25 20:21:31 +0300

    :)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,40 @@
 function redirect(path) {
   if (path == "levels") {
-    window.location.href = `pages/${path}.html`;
+    window.location.href = `pages/${path}.html`
   } else if (path == "book") {
-    window.location.href = `pages/${path}.html`;
+    window.location.href = `pages/${path}.html`
   } else if (path == "dragon-grow") {
-    window.location.href = `pages/${path}.html`;
+    window.location.href = `pages/${path}.html`
   }
 }
 
 const liItems = document.querySelectorAll(".container ul li");
-const gif = document.getElementById("gif");
+const gif = document.getElementById("gif")
 
 for (let i = 0; i < liItems.length; i++) {
   liItems[i].addEventListener("mouseenter", function (e) {
-    const liRect = this.getBoundingClientRect();
+    const liRect = this.getBoundingClientRect()
     const containerRect = document
       .querySelector(".container")
-      .getBoundingClientRect();
-    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top;
-    gif.style.opacity = "1";
-    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`;
-    gif.style.left = `70%`; // Move slightly closer on hover
+      .getBoundingClientRect()
+    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top
+    gif.style.opacity = "1"
+    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`
+    gif.style.left = `73%`
     triangle;
   });
 
   liItems[i].addEventListener("mousemove", function (e) {
-    const liRect = this.getBoundingClientRect();
+    const liRect = this.getBoundingClientRect()
     const containerRect = document
       .querySelector(".container")
-      .getBoundingClientRect();
-    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top;
-    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`;
+      .getBoundingClientRect()
+    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top
+    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`
   });
 
   liItems[i].addEventListener("mouseleave", function () {
-    gif.style.opacity = "0";
-    gif.style.left = `70%`; // Reset to initial position
+    gif.style.opacity = "0"
+    gif.style.left = `73%`
   });
 }
