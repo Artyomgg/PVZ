2aab081e564ab3ec66da8c543fabd606fa1fd84a
nomoroc <yuriydonskoi@gmail.com>
2025-04-28 19:54:43 +0300

    add

--- assets/scripts/1 level.js
+++ assets/scripts/1 level.js
@@ -79,7 +79,7 @@ function updateDragonMenu() {
 		option.addEventListener('click', () => {
 			selectedDragonType = type
 			document.querySelectorAll('.dragon-option').forEach(opt => {
-				opt.style.border = '2px solid #6c1b00'
+				opt.style.border = '2px solid #006c43'
 			})
 			option.style.border = '2px solid #ff4757'
 		})
