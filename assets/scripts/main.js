a614ecf821fe18b7020f16971b95779b5f6f3b4b
babyshark3343 <kahovskag@Gmail.com>
2025-05-01 10:51:58 +0300

    звук - другое меню

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,40 +1,57 @@
 function redirect(path) {
-	if (path == 'levels') {
-		window.location.href = `pages/${path}.html`
-	} else if (path == 'book') {
-		window.location.href = `pages/${path}.html`
-	} else if (path == 'dragon-grow') {
-		window.location.href = `pages/${path}.html`
-	}
+  if (path == "levels") {
+    window.location.href = `pages/${path}.html`;
+  } else if (path == "book") {
+    window.location.href = `pages/${path}.html`;
+  } else if (path == "dragon-grow") {
+    window.location.href = `pages/${path}.html`;
+  }
 }
 
-const liItems = document.querySelectorAll('.container ul li')
-const gif = document.getElementById('gif')
+const liItems = document.querySelectorAll(".container ul li");
+const gif = document.getElementById("gif");
 
 for (let i = 0; i < liItems.length; i++) {
-	liItems[i].addEventListener('mouseenter', function (e) {
-		const liRect = this.getBoundingClientRect()
-		const containerRect = document
-			.querySelector('.container')
-			.getBoundingClientRect()
-		const liCenterY = liRect.top + liRect.height / 2 - containerRect.top
-		gif.style.opacity = '1'
-		gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`
-		gif.style.left = `74%`
-		triangle
-	})
+  liItems[i].addEventListener("mouseenter", function (e) {
+    const liRect = this.getBoundingClientRect();
+    const containerRect = document
+      .querySelector(".container")
+      .getBoundingClientRect();
+    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top;
+    gif.style.opacity = "1";
+    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`;
+    gif.style.left = `74%`;
+    triangle;
+  });
 
-	liItems[i].addEventListener('mousemove', function (e) {
-		const liRect = this.getBoundingClientRect()
-		const containerRect = document
-			.querySelector('.container')
-			.getBoundingClientRect()
-		const liCenterY = liRect.top + liRect.height / 2 - containerRect.top
-		gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`
-	})
+  liItems[i].addEventListener("mousemove", function (e) {
+    const liRect = this.getBoundingClientRect();
+    const containerRect = document
+      .querySelector(".container")
+      .getBoundingClientRect();
+    const liCenterY = liRect.top + liRect.height / 2 - containerRect.top;
+    gif.style.top = `${liCenterY - gif.offsetHeight / 2}px`;
+  });
 
-	liItems[i].addEventListener('mouseleave', function () {
-		gif.style.opacity = '0'
-		gif.style.left = `74%`
-	})
+  liItems[i].addEventListener("mouseleave", function () {
+    gif.style.opacity = "0";
+    gif.style.left = `74%`;
+  });
 }
+
+const sound = new Audio("assets/img/Pyrokinesis_minus.mp3");
+const soundToggleButton = document.querySelector(".soundon");
+sound.volume = 0.1;
+sound.loop = true;
+sound.currentTime = 0;
+
+let isPlaying = false;
+
+soundToggleButton.addEventListener("click", () => {
+  if (isPlaying) {
+    sound.pause();
+  } else {
+    sound.play();
+  }
+  isPlaying = !isPlaying;
+});
