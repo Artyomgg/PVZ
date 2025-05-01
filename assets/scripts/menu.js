8b807311356b4066243091ed41e179ce58cce564
babyshark3343 <kahovskag@Gmail.com>
2025-05-01 10:27:20 +0300

    звук-начальное меню

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,12 +1,32 @@
 const menuButtons = document.querySelectorAll(".menu-button");
 
 const startButton = menuButtons[0];
-const settingsbutton = menuButtons[1];
+const soundToggleButton = menuButtons[1];
+
+const sound = new Audio("assets/img/Pyrokinesis_minus.mp3");
+
+sound.volume = 0.1;
+sound.loop = true;
+sound.currentTime = 0;
+
+let isPlaying = false;
 
 startButton.addEventListener("click", () => {
   window.location.href = "intro.html";
 });
 
-settingsbutton.addEventListener("click", () => {
-  window.location.href = "settings.html";
+soundToggleButton.addEventListener("click", () => {
+  if (isPlaying) {
+    sound.pause();
+    soundToggleButton.textContent = "Включить звук";
+  } else {
+    sound.play();
+    soundToggleButton.textContent = "Выключить звук";
+  }
+  isPlaying = !isPlaying;
+});
+
+const volumeSlider = document.querySelector(".volume-slider");
+volumeSlider.addEventListener("input", () => {
+  sound.volume = volumeSlider.value;
 });
