23db9a4390029ee5d5079d56ea764e8ad1573069
nomoroc <yuriydonskoi@gmail.com>
2025-05-08 17:07:57 +0300

    add gacha

--- assets/scripts/gachaone.js
+++ assets/scripts/gachaone.js
@@ -2,6 +2,15 @@ const egg = document.querySelector('.egg')
 const emeralds = document.querySelector('.emeralds')
 
 egg.addEventListener('click', () => {
-    egg.style.opacity = 0
-  })
-
+    const random = Math.random() * 100
+    
+    if (random < 1) {
+        egg.src = '/assets/img/Dragons/dragonskinone.png'
+    } else if (random < 10) {
+        egg.src = '/assets/img/Dragons/dragonskintwo.png'
+    } else if (random < 10) {
+        egg.src = '/assets/img/Dragons/dragonskinthree.png'
+    } else {
+        egg.style.opacity = 0
+    }
+})  
--- assets/scripts/gachaten.js
+++ assets/scripts/gachaten.js
@@ -2,8 +2,17 @@ const eggs = document.querySelectorAll('.egg');
 const emeralds = document.querySelector('.emeralds');
 
 for (let i = 0; i < eggs.length; i++) {
-  const egg = eggs[i];
+  const random = Math.random() * 100
+    const egg = eggs[i];
     egg.addEventListener('click', () => {
-      egg.style.opacity = 0;
+      if (random < 1) {
+        egg.src = '/assets/img/Dragons/dragonskinone.png'
+    } else if (random < 10) {
+        egg.src = '/assets/img/Dragons/dragonskintwo.png'
+    } else if (random < 10) {
+      egg.src = '/assets/img/Dragons/dragonskinthree.png'
+    } else {
+        egg.style.opacity = 0
+    }
   })
 }
