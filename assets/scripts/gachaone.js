e644210a07d8888fab7d1d09402bfed3013913cf
nomoroc <yuriydonskoi@gmail.com>
2025-05-08 17:41:43 +0300

    заготовка

--- assets/scripts/gachaone.js
+++ assets/scripts/gachaone.js
@@ -1,16 +1,25 @@
 const egg = document.querySelector('.egg')
 const emeralds = document.querySelector('.emeralds')
 
-egg.addEventListener('click', () => {
+let clickone = egg.addEventListener('click', () => {
     const random = Math.random() * 100
     
     if (random < 1) {
         egg.src = '/assets/img/Dragons/dragonskinone.png'
-    } else if (random < 10) {
+        egg.addEventListener('click', () => {
+          alert('клик1')
+        })
+    } else if (random < 11) {
         egg.src = '/assets/img/Dragons/dragonskintwo.png'
-    } else if (random < 10) {
+        egg.addEventListener('click', () => {
+          alert('клик2')
+        })
+    } else if (random < 20) {
         egg.src = '/assets/img/Dragons/dragonskinthree.png'
+        egg.addEventListener('click', () => {
+          alert('клик3')
+        })
     } else {
         egg.style.opacity = 0
     }
-})  
+  })
--- assets/scripts/gachaten.js
+++ assets/scripts/gachaten.js
@@ -7,10 +7,19 @@ for (let i = 0; i < eggs.length; i++) {
     egg.addEventListener('click', () => {
       if (random < 1) {
         egg.src = '/assets/img/Dragons/dragonskinone.png'
-    } else if (random < 10) {
+        egg.addEventListener('click', () => {
+          alert('клик1')
+        })
+    } else if (random < 11) {
         egg.src = '/assets/img/Dragons/dragonskintwo.png'
-    } else if (random < 10) {
-      egg.src = '/assets/img/Dragons/dragonskinthree.png'
+        egg.addEventListener('click', () => {
+          alert('клик2')
+        })
+    } else if (random < 20) {
+        egg.src = '/assets/img/Dragons/dragonskinthree.png'
+        egg.addEventListener('click', () => {
+          alert('клик3')
+        })
     } else {
         egg.style.opacity = 0
     }
