bc8fcc87b6f1789123ad9fa61836a653aa523b6b
nomoroc <yuriydonskoi@gmail.com>
2025-05-08 18:02:53 +0300

    dop

--- assets/scripts/gachaone.js
+++ assets/scripts/gachaone.js
@@ -7,16 +7,19 @@ let clickone = egg.addEventListener('click', () => {
     egg.addEventListener('click', () => {
       if (random < 1) {
         egg.src = '/assets/img/Dragons/dragonskinone.png'
+        egg.style.transform = 'scale(0.95)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин1?')
         })
     } else if (random < 11) {
         egg.src = '/assets/img/Dragons/dragonskintwo.png'
+        egg.style.transform = 'scale(0.80)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин2?')
         })
     } else if (random < 20) {
         egg.src = '/assets/img/Dragons/dragonskinthree.png'
+        egg.style.transform = 'scale(0.80)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин3?')
         })
--- assets/scripts/gachaten.js
+++ assets/scripts/gachaten.js
@@ -7,16 +7,19 @@ for (let i = 0; i < eggs.length; i++) {
     egg.addEventListener('click', () => {
       if (random < 1) {
         egg.src = '/assets/img/Dragons/dragonskinone.png'
+        egg.style.transform = 'scale(0.95)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин1?')
         })
     } else if (random < 11) {
         egg.src = '/assets/img/Dragons/dragonskintwo.png'
+        egg.style.transform = 'scale(0.80)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин2?')
         })
     } else if (random < 20) {
         egg.src = '/assets/img/Dragons/dragonskinthree.png'
+        egg.style.transform = 'scale(0.80)' 
         egg.addEventListener('click', () => {
           confirm('добавить скин3?')
         })
